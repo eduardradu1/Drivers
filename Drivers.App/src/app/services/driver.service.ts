8605baeebcf25975eec10acb6caebcacd30a408d
eduardradu1 <93004185+eduardradu1@users.noreply.github.com>
2023-08-22 18:27:24 +0300

    Updated final version

---
+++ Drivers.App/src/app/services/driver.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Driver }  from '../models/driver';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriverService {
+  url = "Drivers"
+  constructor(private http: HttpClient) { }
+
+  public getDrivers() : Observable<Driver[]> {
+    return this.http.get<Driver[]>(`${environment.apiUrl}/${this.url}`);
+  }
+}
